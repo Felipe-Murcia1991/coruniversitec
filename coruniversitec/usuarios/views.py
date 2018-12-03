50507f2282636ac730850986f046929c591703ba
FelipeMfc10 <felipe.embajador1991@gmail.com>
2018-12-03 08:56:30 -0500

    suprimir import
    se eliminan librerias

--- coruniversitec/usuarios/views.py
+++ coruniversitec/usuarios/views.py
@@ -1,8 +1,5 @@
 from django.http import HttpResponse
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCerationsForm
-from django.views.generic import CreateView
-from djanfo.core.urlresolvers import reverse_lazy
+
 
 
 # Create your views here.
@@ -10,10 +7,3 @@ from djanfo.core.urlresolvers import reverse_lazy
 
 def Bienvenidos(request):
     return HttpResponse("Bienvenidos a SDAF")
-
-
-class RegistroUsuario(CreateView):
-    model=UserCerationsForm
-    template_name="usuarios/registro.html"
-    form_class=UserCerationsForm
-    success_url=reverse_lazy
