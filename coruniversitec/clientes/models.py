84f31ed97daacc1f2b66af09498ab8fed7120652
FelipeMfc10 <felipe.embajador1991@gmail.com>
2018-12-03 17:25:20 -0500

    correccion y adicion
    se corrigen tipos de datos en clase Cliente y se adiciona templates con archivos booptras para personalizacion en js y css

--- coruniversitec/clientes/models.py
+++ coruniversitec/clientes/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Cliente(models.Model):
-    folio=models.CharField(max_length=10,primary_key=True)
+    folio=models.IntegerField(primary_key=True)
     documento=models.CharField(max_length=15)
     nombre=models.CharField(max_length=50)
     apellido=models.CharField(max_length=50)
-    telefono=models.IntegerField()
+    telefono=models.CharField(max_length=50)
     edad=models.IntegerField()
     correo=models.EmailField()
-    
+    postulado=models.CharField(max_length=50)
 
 class Buscar(models.Model):
     folio=models.ForeignKey(Cliente, on_delete=models.CASCADE)
