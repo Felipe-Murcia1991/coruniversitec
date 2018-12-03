d2c7598b233b55a622b0b025d32dce3b479e5e90
FelipeMfc10 <felipe.embajador1991@gmail.com>
2018-12-03 15:17:52 -0500

    tipo de dato
    se cambia el tipo de dato en el campo telefono a char

--- coruniversitec/usuarios/models.py
+++ coruniversitec/usuarios/models.py
@@ -5,7 +5,7 @@ class Empleado(models.Model):
     documento=models.IntegerField()
     nombre=models.CharField(max_length=50)
     apellido=models.CharField(max_length=50)
-    telefono=models.IntegerField()
+    telefono=models.CharField(max_length=15)
     cargo=models.CharField(max_length=50)
     correo=models.EmailField()
 
