13cf55dfa143087190aaf3578d9f1499777ced13
FelipeMfc10 <felipe.embajador1991@gmail.com>
2018-12-03 11:40:52 -0500

    Revert "columna correo"
    This reverts commit a4632ae0082ed03853b9be87c6824e8b415b643b.

--- coruniversitec/usuarios/models.py
+++ coruniversitec/usuarios/models.py
@@ -2,15 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Empleado(models.Model):
-    id = models.AutoField(primary_key=True)
     documento=models.IntegerField()
     nombre=models.CharField(max_length=50)
     apellido=models.CharField(max_length=50)
     telefono=models.IntegerField()
     cargo=models.CharField(max_length=50)
-    correo=models.EmailField()
+    coreo=models.EmailField()
 
 class Elegir(models.Model):
-    
     documento=models.ForeignKey(Empleado, on_delete=models.CASCADE)
     nombre=models.CharField(max_length=50)
