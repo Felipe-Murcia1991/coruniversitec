f7dc85c72a6e7040283cb53a0aa40911fc43e8f1
FelipeMfc10 <felipe.embajador1991@gmail.com>
2018-12-03 09:14:27 -0500

    modificacion archivos conectores de urls

--- coruniversitec/usuarios/urls.py
+++ coruniversitec/usuarios/urls.py
@@ -5,9 +5,7 @@ from . import views
 
 urlpatterns=[
     url(
-        regex=r'^Bienvenidos/$',
-        view=views.Bienvenidos,
-        name='Bienvenidos_a _SDAF',
+        path('', views.Bienvenidos, name='Bienvenidos'),
     )
 
 ]
