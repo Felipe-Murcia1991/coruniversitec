3241ab9e12a798a1a037c3a8d21356cf26337a5a
FelipeMfc10 <felipe.embajador1991@gmail.com>
2018-12-03 16:46:42 -0500

    correciones de sintaxis y adicion de clases

--- coruniversitec/clientes/models.py
+++ coruniversitec/clientes/models.py
@@ -3,9 +3,14 @@ from django.db import models
 # Create your models here.
 class Cliente(models.Model):
     folio=models.CharField(max_length=10,primary_key=True)
-    documento=models.IntegerField()
+    documento=models.CharField(max_length=15)
     nombre=models.CharField(max_length=50)
     apellido=models.CharField(max_length=50)
     telefono=models.IntegerField()
     edad=models.IntegerField()
     correo=models.EmailField()
+    
+
+class Buscar(models.Model):
+    folio=models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    documento=models.CharField(max_length=15)
--- coruniversitec/clientes/views.py
+++ coruniversitec/clientes/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
+
+
+# Create your views here.
+#primera impresion con funcion
+
+def RegistroCliente(request):
+    return HttpResponse("Registro de clientes")
 # Create your views here.
